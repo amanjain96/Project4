d4044888d161f3db19d26160b74bbd62f24e55fe
Aman Jain <akj2137@columbia.edu>
2019-04-14 16:12:09 +0000

    All functions added

--- README.txt
+++ README.txt
@@ -1,2 +1,3 @@
 #Group4
 This is a first edit
+load.py syntax seems fine.
--- classify.py
+++ classify.py
@@ -1,9 +1,39 @@
 def love(lyrics):
     love_count = 0
     for word in lyrics.split():
-        if word in ['love', 'kiss', 'dream']:
+        if word in ['love', 'kiss', 'dream','amour','yearning','sex','like','babe','baby','girlfriend','boyfriend']:
             love_count += 1
-        else:
-            continue
     
     return love_count
+
+
+def kid_safe(lyrics):
+    kid_count = 0
+    for word in lyrics.split():
+        if word in ['fuck','bitch','shit','ass','asshole','fag','jerkoff','whore','cunt','pussy','arse']
+            kid_count = 0
+
+    return kid_count
+
+def length(lyrics):
+    length = 0
+    for word in lyrics.split():
+        length += 1
+
+    return length
+
+def complex(lyrics):
+    complexity = 0
+    for word in lyrics.split():
+        if word in ['']: #we need to split in lines instead of words and check metaphors, alliterations,etc
+            complexity +=1
+
+    return complexity
+
+def mood(lyrics):
+    mood = 0
+    for word in lyrics.split():
+        if word in ['upbeat','happy','brilliant','energetic', 'enthusiastic']:
+            mood +=1
+
+    return mood
