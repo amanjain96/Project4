6e8d9914a563afec37139cad8610733dd51457af
Mitchell Sitzer <mts2180@columbia.edu>
2019-04-20 18:13:23 +0000

    Format classify.py

--- classify.py
+++ classify.py
@@ -1,38 +1,38 @@
+def kid_safe(lyrics):
+	kid_count = 0
+	for word in lyrics.split():
+		if word in ['fuck', 'bitch', 'shit', 'ass', 'asshole', 'fag', 'jerkoff', 'whore', 'cunt', 'pussy', 'arse']:
+			kid_count += 1
+
+	return kid_count
+
 def love(lyrics):
     love_count = 0
     for word in lyrics.split():
-        if word in ['love', 'kiss', 'dream','amour','yearning','sex','like','babe','baby','girlfriend','boyfriend']:
+        if word in ['love', 'kiss', 'dream', 'amour', 'yearning', 'sex', 'like', 'babe', 'baby', 'girlfriend', 'boyfriend']:
             love_count += 1
     
     return love_count
 
-def kid_safe(lyrics):
-    kid_count = 0
-    for word in lyrics.split():
-        if word in ['fuck','bitch','shit','ass','asshole','fag','jerkoff','whore','cunt','pussy','arse']
-            kid_count = 0
+def mood(lyrics):
+	mood_count = 0
+	for word in lyrics.split():
+		if word in ['upbeat', 'happy', 'brilliant', 'energetic', 'enthusiastic']:
+			mood_count += 1
 
-    return kid_count
+	return mood_count
 
 def length(lyrics):
-    length = 0
+    length_count = 0
     for word in lyrics.split():
         length += 1
 
-    return length
+    return length_count
 
-def complex(lyrics):
-    complexity = 0
+def complexity(lyrics):
+    complexity_count = 0
     for word in lyrics.splitlines():
         if word in ['is like','yo yo yo','uptown funk']: #we need to split in lines instead of words and check metaphors, alliterations,etc
-            complexity +=1
+            complexity_count +=1
 
-    return complexity
-
-def mood(lyrics):
-    mood = 0
-    for word in lyrics.split():
-        if word in ['upbeat','happy','brilliant','energetic', 'enthusiastic']:
-            mood +=1
-
-    return mood
+	return complexity_count
