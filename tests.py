28e9095d012c8a4d50c3572bba578557680cdef3
amanjain96 <48304390+amanjain96@users.noreply.github.com>
2019-05-05 15:40:26 -0400

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,14 +1,12 @@
-
 #This file contains the Unit-testing required to test our code
 import unittest
 
 
 #Unit testing for non-english song
 class TestNonEnglish(unittest.TestCase):
-   # def test_kidsafe(self):
-#    for song_id in non_english:
-#            self.assertDictEqual(song_classification,{'kid_safe':0.5,'love':0.5,'mood',0.5,'complexity':0.5})
-    pass    
+    def test_kidsafe(self):
+        for song_id in non_english:
+            self.assertDictEqual(song_classification,{'kid_safe':0.5,'love':0.5,'mood',0.5,'complexity':0.5})    
 
 class TestClassify(unittest.TestCase):
 
@@ -20,34 +18,30 @@ class TestClassify(unittest.TestCase):
 
     def test_title(self):
         self.assertEqual(classify.song_title('45~Michael-Jackson~Billie-Jean'),'Billie Jean')
-
+        
     def test_kidsafe1(self):
-        self.assertEqual(classify.kid_safe('This is a kid friendly song with good words',1)
-
+        self.assertEqual(classify.kid_safe('This is a kid friendly song with good words',1))
+                         
     def test_kidsafe2(self):
-        self.assertEqual(classify.kid_safe('I am so ruined, fucked. **** Life sucks.',0.6)
-   
+        self.assertEqual(classify.kid_safe('I am so ruined, fucked. **** Life sucks.',0.6))
+                         
     def test_love1(self):
-        self.assertEqual(classify.love('This is a kid friendly song with good words',0)
-
+        self.assertEqual(classify.love('This is a kid friendly song with good words',0))
+                         
     def test_love2(self):
-        self.assertEqual(classify.love('Habibi, I love you, I need you, I miss you,I wanna kiss you. Oh babe!',0.6)
-
-    
+        self.assertEqual(classify.love('Habibi, I love you, I need you, I miss you,I wanna kiss you. Oh babe!',0.6))
+        
 class TestInputOutput(unittest.TestCase):
-
+                         
     def test_output(self):
         pass
 
-
     def test_input(self):
-
         with self.assertRaises():
             main()
-
+        
         with self.assertRaises():
             main('incorrect path format')
-
 
 class TestArgeParser(unittest.TestCase):
 
@@ -58,9 +52,5 @@ class TestArgeParser(unittest.TestCase):
         self.assertIn("if __name__ == '__main__':", main.py)
 
 
-
-
 if __name__ == '__main__':
     unittest.main()
-
-
