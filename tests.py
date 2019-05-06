fe59c920abaa54a7c07360e13e2516994934ef51
Mitchell Sitzer <mts2180@columbia.edu>
2019-05-06 01:32:07 -0400

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,7 +1,6 @@
 # This file contains the Unit-testing required to test our code
 import unittest
 import main
-import load
 import classify
 
 # Unit testing for non-english song
@@ -10,51 +9,48 @@ import classify
 #        for song_id in non_english:
 #            self.assertDictEqual(song_classification,{'kid_safe':0.5,'love':0.5,'mood',0.5,'complexity':0.5})    
 
-class TestClassify(unittest.TestCase):
+class TestClassifyMethods(unittest.TestCase):
+    
+    def test_song_id(self):
+        self.assertEqual(classify.song_id('45~Michael-Jackson~Billie-Jean.txt'), 45)
 
-    def test_id(self):
-        self.assertEqual(classify.song_id('45~Michael-Jackson~Billie-Jean'),45)
+    def test_song_artist(self):
+        self.assertEqual(classify.song_artist('45~Michael-Jackson~Billie-Jean.txt'), 'Michael Jackson')
 
-    def test_artist(self):
-        self.assertEqual(classify.song_artist('45~Michael-Jackson~Billie-Jean'),'Michael Jackson')
-
-    def test_title(self):
-        self.assertEqual(classify.song_title('45~Michael-Jackson~Billie-Jean'),'Billie Jean')
+    def test_song_title(self):
+        self.assertEqual(classify.song_title('45~Michael-Jackson~Billie-Jean.txt'), 'Billie Jean')
         
-    def test_kidsafe1(self):
-        self.assertEqual(classify.kid_safe('This is a kid friendly song with good words'),1)
-
-    def test_kidsafe2(self):
-        self.assertEqual(classify.kid_safe("I am so ruined, fucked  Life sucks"),0.7)
+    def test_kid_safe(self):
+        self.assertEqual(classify.kid_safe('This is a kid friendly song with good words'), 1)
+        self.assertEqual(classify.kid_safe('I am so ruined, fuck life sucks'), 0.7)
+        self.assertEqual(classify.kid_safe('Cuss words include fuck, fucking, fucker, shit, bitch, bitches, ass, asshole, mothafucka, and whore'), 0)
                          
-    def test_love1(self):
-        self.assertEqual(classify.love('This is a kid friendly song with good words'),0)
-                         
-    def test_love2(self):
-        self.assertEqual(classify.love('Habibi, I love you, I miss you, I wanna kiss you. Oh babe'),0.5)
+    def test_love(self):
+        self.assertEqual(classify.love('This is a kid friendly song with good words'), 0)
+        self.assertEqual(classify.love('Habibi, I love you, I miss you, I wanna kiss you. Oh babe'), 0.5)
     
     def test_mood(self):
-        self.assertEqual(classify.mood('I am so happy and energetic but dead'),0.6)
+        self.assertEqual(classify.mood('I am so happy and energetic but dead'), 0.6)
     
     def test_length(self):
-        self.assertEqual(classify.length('This is a short seven word song'),0)
+        self.assertEqual(classify.length('This is a short seven word song'), 0)
 
     def test_instrumental(self):
         lyrics = '[Instrumental]'
-        self.assertEqual(classify.kid_safe(lyrics),0.5)
-        self.assertEqual(classify.love(lyrics),0.5)
-        self.assertEqual(classify.length(lyrics),0.5)
-        self.assertEqual(classify.mood(lyrics),0.5)
-        self.assertEqual(classify.complexity(lyrics),0.5)
+        self.assertEqual(classify.kid_safe(lyrics), 0.5)
+        self.assertEqual(classify.love(lyrics), 0.5)
+        self.assertEqual(classify.length(lyrics), 0.5)
+        self.assertEqual(classify.mood(lyrics), 0.5)
+        self.assertEqual(classify.complexity(lyrics), 0.5)
 
-class TestInputOutput(unittest.TestCase):
-    
-    def test_output(self):
-        self.assertIsInstance(main.main('/home/akj2137/Project4/lyrics'),str)
-
-    def test_input(self):
-        with self.assertRaises(TypeError):
-            main.main()
+#class TestInputOutput(unittest.TestCase):
+#    
+#    def test_output(self):
+#        self.assertIsInstance(main.main('lyrics'), str)
+#
+#    def test_input(self):
+#        with self.assertRaises(TypeError):
+#            main.main()
 
 if __name__ == '__main__':
     unittest.main()
